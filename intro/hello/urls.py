f19044a9038b3d6936b7bf34d4d9c58d2930be8d
jgrynczewski <jgrynczewski@gmail.com>
2024-06-19 19:02:59 +0200

    user input 1

--- intro/hello/urls.py
+++ intro/hello/urls.py
@@ -5,9 +5,16 @@ from hello import views
 
 urlpatterns = [
     # view intro
+
     path('', views.hello_view),
 
     # templates intro
     path('hi/', views.hi_view),
     path('hi2/', views.hi2_view),
+
+    # user input
+    path('jurek/', views.jurek_view),
+    path('jacek/', views.jacek_view),
+    path('ksawier/', views.ksawier_view),
+    path('<name>/', views.name_view)
 ]
--- intro/hello/views.py
+++ intro/hello/views.py
@@ -22,3 +22,18 @@ def hi_view(request):
 def hi2_view(request):
     return render(request, 'hello.html')
 
+
+def jurek_view(request):
+    return HttpResponse("Witaj, Jurek!")
+
+
+def jacek_view(request):
+    return HttpResponse("Witaj, Jacek!")
+
+
+def ksawier_view(request):
+    return HttpResponse("Witaj, Ksawier!")
+
+
+def name_view(request, name):
+    return HttpResponse(name)
