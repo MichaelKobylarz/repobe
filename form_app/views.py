643bd29b9ac30876d38fa63cc88ef70df4c8bffd
jgrynczewski <jgrynczewski@gmail.com>
2024-07-03 19:36:56 +0200

    formularze + metoda post

--- form_app/views.py
+++ form_app/views.py
@@ -6,11 +6,36 @@ def hello_get(request):
     data = request.GET
 
     name = None
-    if 'name' in data:
-        name = data['name']
+    if 'first_name' in data:
+        name = data['first_name']
 
     return render(
         request,
         'form_app/hello_get.html',
+        {'name': name}
+    )
+
+
+def show(request):
+    data = request.GET
+
+    name = None
+    if 'first_name' in data:
+        name = data['first_name']
+
+    return render(
+        request,
+        'form_app/show.html',
+        {'name': name}
+    )
+
+
+def hello_post(request):
+    data = request.POST
+    name = data.get('first_name')
+
+    return render(
+        request,
+        'form_app/hello_post.html',
         {'name': name}
     )
