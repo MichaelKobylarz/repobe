c134246118c50d6212dc53d1fa5078f8f0f943df
jgrynczewski <jgrynczewski@gmail.com>
2024-07-09 20:09:14 +0200

    orm one_to_one

--- orm_app/models.py
+++ orm_app/models.py
@@ -6,3 +6,12 @@ class Task(models.Model):
 
     def __str__(self):
         return f"{self.name}"
+
+
+class Country(models.Model):
+    name = models.CharField(max_length=255)
+    capital = models.OneToOneField('Capital', on_delete=models.CASCADE)
+
+
+class Capital(models.Model):
+    name = models.CharField(max_length=255)
