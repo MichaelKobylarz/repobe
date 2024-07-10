303b1d9c92dd985fca5e24303e1281bb5fa3421b
jgrynczewski <jgrynczewski@gmail.com>
2024-07-10 18:28:00 +0200

    orm many to many

--- orm_app/models.py
+++ orm_app/models.py
@@ -8,6 +8,7 @@ class Task(models.Model):
         return f"{self.name}"
 
 
+# 1 to 1
 class Country(models.Model):
     name = models.CharField(max_length=255)
     capital = models.OneToOneField('Capital', on_delete=models.CASCADE)
@@ -17,6 +18,7 @@ class Capital(models.Model):
     name = models.CharField(max_length=255)
 
 
+# 1 to many
 class Language(models.Model):
     name = models.CharField(max_length=63)
 
@@ -30,3 +32,19 @@ class Framework(models.Model):
 
     def __str__(self):
         return self.name
+
+
+# many to many
+class Actor(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Movie(models.Model):
+    title = models.CharField(max_length=255)
+    actors = models.ManyToManyField('Actor')
+
+    def __str__(self):
+        return self.title
